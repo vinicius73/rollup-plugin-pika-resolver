e7d42fc100aa118c0048b741997a155bdd5c2afc
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-09-26 12:05:22 -0300

    ✅ test custom cdn

--- tests/pika-resolver.test.js
+++ tests/pika-resolver.test.js
@@ -17,3 +17,18 @@ test('pikaResolver', async () => {
     await resolver.resolveId('package-y')
   ).toEqual(undefined)
 })
+
+test('pikaResolver (custom cdn)', async () => {
+  const resolver = pikaResolver({ modules: ['package-x'], cdnHost: 'https://my.cdn' })
+
+  expect(
+    await resolver.resolveId('package-x')
+  ).toEqual({
+    id: 'https://my.cdn/package-x@1.x.x',
+    external: true
+  })
+
+  expect(
+    await resolver.resolveId('package-y')
+  ).toEqual(undefined)
+})
